c47b80068eca51e01b745194226b79d953cbe5d5
Nathaniel Alcedo <natealcedo@gmail.com>
2024-06-11 15:43:00 +0800

    Add barebones parser

---
+++ parser/parser.go
@@ -0,0 +1,32 @@
+package parser
+
+import (
+	"github.com/natealcedo/monkey/ast"
+	"github.com/natealcedo/monkey/lexer"
+	"github.com/natealcedo/monkey/token"
+)
+
+type Parser struct {
+	l *lexer.Lexer
+
+	curToken  token.Token
+	peekToken token.Token
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+
+	// Read two tokens, so curToken and peekToken are both set
+	p.nextToken()
+	p.nextToken()
+	return p
+}
+
+func (p *Parser) nextToken() {
+	p.curToken = p.peekToken
+	p.peekToken = p.l.NextToken()
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	return nil
+}
