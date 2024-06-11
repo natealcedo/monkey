c16a8dbe581344791736043b8e3adf99639cfc5d
Nathaniel Alcedo <natealcedo@gmail.com>
2024-06-11 13:12:31 +0800

    Add support for indentifiers in token

--- token/token.go
+++ token/token.go
@@ -32,3 +32,15 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdentifier(identifier string) TokenType {
+	if tok, ok := keywords[identifier]; ok {
+		return tok
+	}
+	return IDENT
+}
